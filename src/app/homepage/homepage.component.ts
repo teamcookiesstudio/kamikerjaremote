b4c0d8a0cc8013d1fc6123737f32babfe0ea8a18
Andhika Satya <andhika@mykios.co>
2018-03-11 00:05:32 +0700

    Homepage
    1. add contents
    2. horizontal carousel slider
    3. curvy svg

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {NgxCarousel} from 'ngx-carousel';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit {
+
+  public carouselOne: NgxCarousel;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.carouselOne = {
+      grid: {xs: 1, sm: 1, md: 4, lg: 6, all: 0},
+      slide: 1,
+      speed: 1000,
+      interval: 2000,
+      point: {
+        visible: false
+      },
+      load: 2,
+      touch: false,
+      loop: true,
+      custom: 'banner'
+    }
+  }
+
+  myfunc(event: Event) {
+
+  }
+
+}
---
+++ src/app/landing-nav-bar/landing-nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-nav-bar',
+  templateUrl: './landing-nav-bar.component.html',
+  styleUrls: ['./landing-nav-bar.component.scss']
+})
+export class LandingNavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
